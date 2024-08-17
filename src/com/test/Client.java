32d2a79f30afbbf39272ffd83a6abbad241969c0
SnowCrane <1277456806@qq.com>
2024-08-18 03:41:34 +0800

    socket by UDP

--- src/com/test/Client.java
+++ src/com/test/Client.java
@@ -4,22 +4,22 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
-        try(Socket socket = new Socket("127.0.0.1",8080);
-            Scanner in = new Scanner(System.in)){
-            System.out.println("已经连接到服务端,请输入要传输的文件地址");
-            OutputStream outputStream = socket.getOutputStream();
-            String path = in.nextLine();
-            FileInputStream inputStream = new FileInputStream(path);
-            inputStream.transferTo(outputStream);
-            inputStream.close();
-        }catch(IOException e){
+        try(DatagramSocket socket = new DatagramSocket();
+            Scanner scanner = new Scanner(System.in)) {
+            while(true){
+                String str = scanner.nextLine();
+                byte[] data = str.getBytes();
+                InetAddress address = InetAddress.getByName("localhost");
+                DatagramPacket packet = new DatagramPacket(data, data.length,address,8080);
+                socket.send(packet);
+            }
+        }catch (IOException e) {
             e.printStackTrace();
         }
     }
--- src/com/test/Server.java
+++ src/com/test/Server.java
@@ -4,25 +4,21 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) {
-        try(ServerSocket server = new ServerSocket(8080);
-            FileOutputStream outputStream = new FileOutputStream("xxx.txt")){
-            Socket socket = server.accept();
-            System.out.println("接收来自客户端的连接"+socket.getInetAddress()+":"+socket.getPort());
-            InputStream inputStream = socket.getInputStream();
-            long len,total=0;
-            byte[] buffer=new byte[1024];
-            while((len=inputStream.read(buffer))!=-1){
-                total+=len;
-                System.out.println("文件正在读取，已读取"+total+"字节");
-                outputStream.write(buffer);
-            }
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+       try(DatagramSocket socket = new DatagramSocket(8080)){
+           while(true){
+               DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
+               socket.receive(packet);
+               System.out.println("接收到来自ip"+packet.getAddress()+packet.getPort()+"的数据包："+new String(packet.getData(),packet.getOffset(),packet.getLength()));
+           }
+       }catch(IOException e){
+
+       }
     }
 }
