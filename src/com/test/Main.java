e62fc8b94081920787f4404f82526fc2a0b9d636
SnowCrane <1277456806@qq.com>
2024-08-19 04:05:24 +0800

    preparedStatement

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -1,17 +1,32 @@
 package com.test;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         try (Connection connection = DriverManager.getConnection("jdbc:mysql://192.168.229.129:3306/web_study", "root", "123456");
-             Statement statement = connection.createStatement()) {
-            List<String> user = List.of("小刚","小强","小王","小美","小黑子");
-            for(String name : user){
-                statement.addBatch("insert into user (name,age) values('"+name+"',19)");
+             PreparedStatement statement = connection.prepareStatement("select * from account where name = ? and password = ?");
+             Scanner scanner = new Scanner(System.in)) {
+            System.out.print("请输入用户名：");
+            String username = scanner.nextLine();
+            System.out.print("请输入密码：");
+            String password = scanner.nextLine();
+
+            statement.setString(1,username);
+            statement.setString(2,password);
+
+            ResultSet rs = statement.executeQuery();
+
+            if(rs.next()) {
+                Account account = new Account(rs.getInt(1), rs.getString(2), rs.getString(3));
+                System.out.println("登录成功 账户信息为："+account);
+            }else {
+                System.out.println("登录失败，用户名或密码错误");
             }
-            int[] result = statement.executeBatch();
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- src/com/test/User.java
+++ src/com/test/User.java
@@ -5,5 +5,14 @@ public class User {
     private String name;
     private int age;
 
+    public User(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String say(){
+        return "我是"+id+"号 我的名字是"+name+" 我今年"+age+"了";
+    }
 
 }
