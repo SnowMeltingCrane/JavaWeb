b37db7e71d6456a620af93c647b57ee41f714406
SnowCrane <1277456806@qq.com>
2024-08-20 02:42:16 +0800

    select in Mybatis

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -1,24 +1,19 @@
 package com.test;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import com.test.entity.User;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.sql.*;
-import java.util.ArrayList;
+import java.io.FileInputStream;
 import java.util.List;
-import java.util.Scanner;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        Document document = builder.parse("file:text.xml");
-        NodeList list = document.getElementsByTagName("name");
-        Node item = list.item(0);
-        System.out.println(item.getTextContent());
+    public static void main(String[] args) {
+        try(SqlSession session = MybatisUtils.openSession(true)){
+            Map<String,User> map = session.selectMap("selectUsersByAge",20,"id");
+            System.out.println(map);
+        }
     }
 }
---
+++ src/com/test/entity/User.java
@@ -0,0 +1,16 @@
+package com.test.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class User {
+    private int id;
+    private String name;
+    private int age;
+}
+
+
